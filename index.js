7ed162ecdb35adc97480f7b87959aca2c8646d2d
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2021-01-11 16:21:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let prevBtn = document.querySelector('.prev');
+let nextBtn = document.querySelector('.next');
+let slider = document.querySelector('.slider');
+
+let currentImage = 1;
+slider.src = `./src/img/${currentImage}.jpg`;
+
+onPrevBtn = () => {
+  console.log('prev');
+}
+onNextBtn = () => {
+  console.log('next');
+}
+
+prevBtn.addEventListener('click', onPrevBtn);
+nextBtn.addEventListener('click', onNextBtn);
